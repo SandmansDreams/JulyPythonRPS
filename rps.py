99cfcfac8e16082b9f346e166c7aebbe45e7f9c9
ryanjrman <thesandmansdreams@gmail.com>
2026-07-23 09:47:54 -0600

    Post-class improvements

--- rps.py
+++ rps.py
@@ -11,6 +11,38 @@
 import random
 
 # Helpers
+def convertToNumber(string: str) -> int: # Convert input to a number
+    choice = string.strip()
+
+    if choice in ["rock", "r", "1"]:
+        return 1
+    elif choice in ["paper", "p", "2"]:
+        return 2
+    elif choice in ["scissors", "s", "3"]:
+        return 3
+    elif choice in ["quit", "q", "exit", "e", "4"]:
+        return 4
+    else: # Should not be possible
+        return 0 
+    
+def compare(a, b) -> int: # Compare two ints to determine a winner
+    # Tie
+    if a == b:
+        return 1
+    # Cases where player wins
+    elif a == 1 and b == 3: 
+        return 2
+    elif a == 2 and b == 1: 
+        return 2
+    elif a == 3 and b == 2: 
+        return 2
+    # Quit
+    elif a == 4:
+        return 4
+    # Cases where player loses
+    else:
+        return 3
+
 def convertToPlay(number: int) -> str: # Converts an int back into a string representing the play
     if number == 1: 
         return "Rock"
@@ -18,54 +50,48 @@ def convertToPlay(number: int) -> str: # Converts an int back into a string repr
         return "Paper"
     elif number == 3:
         return "Scissors"
-    else:
+    elif number == 4:
+        return "Quit"
+    else: # Should not be posible
         return "Unreadable"
-
-def compare(a, b) -> int: # Compare two ints to determine a winner
-    if a == b:
-        return 0
-    elif a == 1 and b == 3: 
-        return 1
-    elif a == 2 and b == 1: 
-        return 1 
-    elif a == 3 and b == 2: 
-        return 1
-    else:
-        return 2
 
 # Main
 def main():
-    computerChoice = random.randint(0, 2) # Get random choice
-    userChoice = input("Rock, paper, or scisors?: ").lower()
+    gameon = True
 
-    def convertToNumber(string: str) -> int:
-        choice = string.strip()
+    while gameon:
+        print("")
+        computerChoice = random.randint(1, 3) # Get random choice
+        userChoice = ""
 
-        if choice == "rock" or choice == "r" or choice == "1":
-            return 1
-        elif choice == "paper" or choice == "p" or choice == "2":
-            return 2
-        elif choice == "scissors" or choice == "s" or choice == "3":
-            return 3
-        else:
-            print("Sorry, that didn't make any sense...")
-            return 0
+        validChoice = False
+        while not validChoice:
+            userChoice = input("Rock, paper, or scisors?: ").lower()
+            if userChoice in ["rock", "r", "1", "paper", "p", "2", "scissors", "s", "3", "quit", "q", "exit", "e", "4"]:
+                validChoice = True
+            else:
+                print("Sorry that didn't make sense... try again!")
 
+        converted = convertToNumber(userChoice)
+        condition = compare(converted, computerChoice)
 
-    converted = convertToNumber(userChoice)
-    condition = compare(converted, computerChoice)
+        userPlay = convertToPlay(converted)
+        computerPlay = convertToPlay(computerChoice)
 
-    userPlay = convertToPlay(converted)
-    computerPlay = convertToPlay(computerChoice)
+        print("Computer Choice: " + computerPlay)
+        print("Player Choice: " + userPlay)
 
-    print("Computer Choice: " + computerPlay)
-    print("Player Choice: " + userPlay)
-
-    if condition == 1:
-        print("Player has won! " + userPlay + " beats " + computerPlay)
-    elif condition == 0:
-        print("It's a Tie! " + computerPlay + " ties with " + userPlay)
-    elif condition == 2:
-        print("Computer has won! " + computerPlay + " beats " + userPlay)
+        if condition == 0:
+            print("Your input didn't make sense. Computer won!")
+        elif condition == 1:
+            print("It's a Tie! " + computerPlay + " ties with " + userPlay)
+        elif condition == 2:
+            print("Player has won! " + userPlay + " beats " + computerPlay)
+        elif condition == 3:
+            print("Computer has won! " + computerPlay + " beats " + userPlay)
+        elif condition == 4:
+            gameon = False
+            print("")
+            print("Thanks for playing!")
 
 main()
